159a874ca98a37a0b6d833ba016766e31498bc14
Adriel Santoso <santoso.adriel.imaran.q1@dc.tohoku.ac.jp>
2024-04-17 16:48:05 +0900

    Added .py and sprites files

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import orbit_mania
+import random
+import time
+
+class AppMain:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = AppMain()
+    app.run()
---
+++ orbit_mania.py
@@ -0,0 +1,14 @@
+import pygame
+import math
+
+class World():
+    pass
+
+class Planet():
+    pass
+
+class Player():
+    pass
+
+class Obstacle():
+    pass
