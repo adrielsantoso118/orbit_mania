e3d909e2e141272320e59b3abc506607eb5cb050
Adriel Santoso <santoso.adriel.imaran.q1@dc.tohoku.ac.jp>
2024-04-17 16:52:47 +0900

    orbit_mania.py - Updated World class

--- orbit_mania.py
+++ orbit_mania.py
@@ -2,7 +2,20 @@ import pygame
 import math
 
 class World():
-    pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+
+        # Load images
+        self.background_image = pygame.image.load("background.png")
+        self.planet_image = pygame.image.load("planet.png")
+        self.player_image = pygame.image.load("player.png")
+        self.obstacle_image = pygame.image.load("obstacle.png")
+        self.explosion_image = pygame.image.load("explosion.png")
+
+        # Application
+        pygame.display.set_caption("Orbit Mania")
 
 class Planet():
     pass
